bf91762f8f089b171c91703d10f1a3025d82a73e
Wesley Corner <wescorner@gmail.com>
2022-07-07 23:57:12 +0000

    refactored to be one function with nested callbacks

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const { fetchyMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
-// fetchyMyIP((error, ip) => {
-//   if (error) return console.log("Didn't work:", error);
-//   console.log("It worked:", ip);
-// });
+const { nextISSTimesForMyLocation } = require("./iss");
 
-// fetchCoordsByIP("174.95.70.199", (error, data) => {
-//   if (error) return console.log(error);
-//   console.log(data);
-// });
-
-// fetchISSFlyOverTimes(
-//   { latitude: 43.653226, longitude: -79.3831843 },
-//   (error, data) => {
-//     if (error) return console.log(error);
-//     console.log(data);
-//   }
-// );
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) return console.log(error);
+  for (const time of passTimes) {
+    const d = new Date(time.risetime * 1000);
+    console.log(`${d} for ${time.duration} seconds`);
+  }
+});
--- iss.js
+++ iss.js
@@ -1,35 +1,28 @@
 const request = require("request");
 
-// const fetchyMyIP = function (callback) {
-//   request("https://api.ipify.org?format=json", (error, response, body) => {
-//     if (error) return callback(error, null);
-//     if (response.statusCode !== 200)
-//       return callback(Error(response.statusCode), null);
-//     return callback(null, JSON.parse(body).ip);
-//   });
-// };
-
-// const fetchCoordsByIP = function (ip, callback) {
-//   request(`https://ipwho.is/${ip}`, (error, response, body) => {
-//     const parsed = JSON.parse(body);
-//     if (error) return callback(error, null);
-//     if (!parsed.success) return callback(parsed.message, null);
-//     const latitude = parsed.latitude;
-//     const longitude = parsed.longitude;
-//     return callback(null, { latitude, longitude });
-//   });
-// };
-
-// const fetchISSFlyOverTimes = function (coords, callback) {
-//   const lat = coords.latitude;
-//   const lon = coords.longitude;
-//   request(
-//     `https://iss-pass.herokuapp.com/json/?lat=${lat}&lon=${lon}`,
-//     (error, response, body) => {
-//       if (error) return callback(error, null);
-//       if (body === "invalid coordinates") return callback(body, null);
-//       callback(null, JSON.parse(body).response);
-//     }
-//   );
-// };
-module.exports = { fetchyMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+const nextISSTimesForMyLocation = function (callback) {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200)
+      return callback(Error(response.statusCode), null);
+    request(
+      `https://ipwho.is/${JSON.parse(body).ip}`,
+      (error, response, body) => {
+        const parsed = JSON.parse(body);
+        if (error) return callback(error, null);
+        if (!parsed.success) return callback(parsed.message, null);
+        const lat = parsed.latitude;
+        const lon = parsed.longitude;
+        request(
+          `https://iss-pass.herokuapp.com/json/?lat=${lat}&lon=${lon}`,
+          (error, response, body) => {
+            if (error) return callback(error, null);
+            if (body === "invalid coordinates") return callback(body, null);
+            callback(null, JSON.parse(body).response);
+          }
+        );
+      }
+    );
+  });
+};
+module.exports = { nextISSTimesForMyLocation };
